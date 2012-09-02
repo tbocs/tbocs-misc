a1ea9eb4a1a5e5a96f215a4c647d8779ac51b57a
Sam Zhai <sam@tbocs.org>
2012-09-01 19:21:32 -0700

    signal support

--- popup_output_terminal.cpp
+++ popup_output_terminal.cpp
@@ -1,7 +1,7 @@
 /*============================================================================*\
 
   popup_output_terminal.cpp
-    Open a new terminal for output using FIFO.
+    Open a new terminal for output using FIFO. Linux & Gnome required.
 
   Copyright (C) 2012 Sam Zhai - The Beauty of Computer Science (sam@tbocs.org)
 
@@ -26,14 +26,23 @@
   DEALINGS IN THE SOFTWARE.
 \*============================================================================*/
 
+// Comment this line to use C libraries instead.
+#define USE_CXX
+
 #include <iostream>
 #include <sstream>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+#ifdef USE_CXX
+  #include <fstream>
+#else
+  #include <unistd.h>
+  #include <fcntl.h>
+#endif
 
 using namespace std; 
 
@@ -41,45 +50,81 @@ class Main {
  public:
   string str_location;
   stringstream ss;
-  int counter, pipefd;
-  
-  Main () : str_location("/tmp/testing"), counter(0), pipefd(-1) {
+  int counter;
+
+#ifdef USE_CXX
+  fstream os_fifo;
+#else
+  int fd_fifo;
+#endif
+
+  // Create FIFO and initialize file handle.
+  Main () : str_location("/tmp/testing"), counter(0) {
     mkfifo(str_location.c_str(), S_IFIFO | 0666);
+    open_output_terminal();
 
-    open_output_window();
+#ifdef USE_CXX
+    os_fifo.open(str_location.c_str(), ios_base::in | ios_base::out);
+#else
+    while ((fd_fifo = open(str_location.c_str(), O_RDWR|O_NONBLOCK)) == -1);
+#endif
+    cout << " == Press CTRL+C to quit == " << endl;
+  }
 
-    while ((pipefd = open(str_location.c_str(), O_RDWR|O_NONBLOCK)) == -1);
-    cout << " == Press CTRL + C to quit == " << endl;
-
+  // Feed FIFO input periodically.
+  void print_loop () {
     while (true) {
       sleep(1);
       ss << "Output No. " << counter++ << "\n" ;
-      write(pipefd, ss.str().c_str(), strlen(ss.str().c_str()));
+
+#ifdef USE_CXX
+      os_fifo << ss.str();
+      os_fifo.flush();
+#else
+      write(fd_fifo, ss.str().c_str(), strlen(ss.str().c_str()));
+#endif
       cout << "Sent: " << ss.str();
       ss.str("");
     }
   }
 
+  // Clean up before quitting.
   ~Main () {
-    close(pipefd);
-    system(str_location.insert(0, "rm ").c_str());
+#ifdef USE_CXX
+    os_fifo.close();
+#else
+    close(fd_fifo);
+#endif
+    system(str_location.insert(0, "rm -f ").c_str());
   }
 
-  void open_output_window() {
+  // Open new terminal for displaying FIFO output.
+  void open_output_terminal() {
     ss << "gnome-terminal -e 'bash -c \"while true; do \
              if read -t 0.5 line <> " << str_location << "; then \
                echo Received: $line; \
              else \
                  kill -0 " << getpid() << "; \
-                 if [ $? -ne 0 ]; then \
-                   rm " << str_location << "; exit; fi; \
+                 if [ $? -ne 0 ]; then exit; fi; \
              fi; done;\" ' &";
     system(ss.str().c_str());
     ss.str("");
   }
-};
-  
+} *m = NULL;
+
+// Clean-up enforced.
+void signal_callback (int sig) {
+  if (m != NULL) {
+    delete m;
+  }
+  exit(0);
+}
+
 int main (int argc, char** argv) {
-  Main m;
+  signal(SIGTERM, signal_callback);
+  signal(SIGINT, &signal_callback);
+  m = new Main();
+  m->print_loop();
+  delete m;
   return 0;
 }
